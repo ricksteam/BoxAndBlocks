c15825b17baf800f8400057843f576c0cfc73fdc
Brandon Lacy <blacy1551@gmail.com>
2018-05-29 14:15:59 -0500

    Player localization attempt
    Created script to localize player. Worked however, box ended up outside boundaries.

---
+++ Assets/Resources/Scripts/CenterPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterPlayer : MonoBehaviour {
+
+    public Transform player;
+    public GameObject box;
+	// Use this for initialization
+	void Start () {
+        //box.transform.position = new Vector3(player.transform.position.x + 0.5f, box.transform.position.y, box.transform.position.z);
+
+	}
+	
+	
+}
