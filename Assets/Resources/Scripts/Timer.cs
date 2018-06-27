d4322a68d0657de81d1d74d5095d7d29034570c5
cyberboy1551 <blacy1551@gmail.com>
2018-06-27 13:32:57 -0500

    Added Count up Timer

---
+++ Assets/Resources/Scripts/Timer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    float _timer;
+	// Use this for initialization
+	void Start () {
+        _timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _timer += Time.deltaTime;
+	}
+}
